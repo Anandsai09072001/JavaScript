0095638f8732ca1b4e92f379daa1a88281d9ed54
Anandsai09072001 <158557220+Anandsai09072001@users.noreply.github.com>
2024-02-03 07:50:14 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3)//node me iska error aaja par chrome or browser pe nahi aata
+// java script ka engine browser ke ander hota hai
+console.log(3
+    +
+    3);//this code runs fine but readablity is a thing which you need to take care of as a good programmer.
+//code readablity should be high
+
+// mdn is good source for reference but tc39.es is the official website to learn js(ecma script)
+
+//Data types 
+//number,bigint,string="",boolean=true/false,null=> stand alone value;
+// undefined 
+// symbol =>to find uniqueness we use symbol
+
+//object
+
+//typeof null => object  ......typeof undefined => undefined
+
