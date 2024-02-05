112b36cab85fa280ce109e9deaad8bdeaa18711d
Anandsai09072001 <158557220+Anandsai09072001@users.noreply.github.com>
2024-02-05 05:08:11 +0000

    comparision

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+//comparisons convert null to Number, treating it as 0.that's why null>=0 is true and null>0 is false
+// === (not only it checks the value it also check the datatypes)
+
+//Datatypes:-primitive,non-primitive
+
+// primitive
+// string,Number,symbol,Boolean, null, undefined, symbol, BigInt
+
+// non primitive
+// Array, object, funtions
+
+const heros=["shaktimaam","hero"];//array
+
+let myobj={
+    name:"Anand",
+    age:22,
+};
+
+const myfunction = function(){
+    console.log("Hello world");
+}
+
+
+
