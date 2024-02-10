97454d17fc21bf417ee37ee2e2a16a94e607ab9f
Anandsai09072001 <158557220+Anandsai09072001@users.noreply.github.com>
2024-02-10 09:24:25 +0000

    DatesandTimes

---
+++ 01_basics/06_datesin.js
@@ -0,0 +1,19 @@
+/*let myDates = new Date();
+console.log(myDates.toString());
+console.log(myDates.toLocaleString());
+console.log(typeof(myDates));
+
+let myCreatedDate = new Date(2023,0,23);
+console.log(myCreatedDate.toDateString());
+
+let myCreateddDate = new Date(2023,0,23);
+console.log(myCreateddDate.toLocaleString());
+*/
+
+// time stamps are used when we need to know who has answerd early in contest.
+//let myCreateddDate = new Date(2023,0,23);
+
+//let myTimeStamp = Date.now();
+
+let newDate = new Date();
+console.log(newDate);
